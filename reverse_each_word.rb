7e92f3614e3f55de98d663567f56700963e5c76f
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-21 18:41:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   new_array = []
   array = string.split
-  new_array << array.collect do |word| word.reverse
+  array.collect do |word| new_array << word.reverse
 end
 new_array
 end
